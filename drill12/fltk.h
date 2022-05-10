d5a156b5599deb7db33549342ba3c11302b99eef
jooszebi26 <43747192+jooszebi26@users.noreply.github.com>
2022-05-10 23:25:39 +0200

    Add files via upload

---
+++ drill12/fltk.h
@@ -0,0 +1,17 @@
+#ifndef FLTK_GUARD
+#define FLTK_GUARD 1
+
+#include "fltk/FL/Fl.H"
+#include "fltk/FL/Fl_Window.H" 
+#include "fltk/FL/Fl_Button.H" 
+#include "fltk/FL/Fl_Input.H" 
+#include "fltk/FL/Fl_Output.H" 
+#include <cstdlib>
+#include "fltk/FL/fl_draw.H"
+#include "fltk/FL/Enumerations.H"
+
+#include "fltk/Fl/Fl_JPEG_Image.H"
+#include "fltk/Fl/Fl_GIF_Image.H"
+
+#endif
+
